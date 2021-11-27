4a87b55aada56529b000ff6a7b242eeae579b854
sebastian beltran <sebastian199502@hotmail.com>
2021-11-27 08:34:39 -0500

    ejercicio

--- ejercicio23.php
+++ ejercicio23.php
@@ -1,7 +1,12 @@
 <?php
 $txtNombre="";
+$rdg_lenguaje="";
 if($_POST){
     $txtNombre=(isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
+    $rdg_lenguaje=(isset($_POST['lenguaje']))?$_POST['lenguaje']:"";
+
+    // print_r($_POST);
+    // print_r($rdg_lenguaje);
 }
 ?>
 <!DOCTYPE html>
@@ -14,10 +19,18 @@ if($_POST){
 </head>
 <body>
     <?php if($_POST){ ?>
-   <strong>Hola</strong>:  <?php echo $txtNombre;?>
+   <strong>Hola</strong>:  <?php echo $txtNombre;?> <br>
+   <strong>El lenguaje que te gsuta es:</strong>:  <?php echo $rdg_lenguaje;?> <br>
    <?php }?>
     <form action="ejercicio23.php" method="POST">
-        <input type="text" name="txtNombre" id="">
+        Nombre:<br>
+        <input value="<?php echo  $txtNombre;?>" type="text" name="txtNombre" id=""> <!-- con el value le estamos diciedno que conserve el valor que esta imprimiendo -->
+        <br>
+        <br>
+        ¿te gusta? <br>
+        <br> php :  <input type="radio" <?php echo ($rdg_lenguaje == "php")?"checked":"";?> name="lenguaje" value="php" id=""> <br>
+        <br> html : <input type="radio" <?php echo ($rdg_lenguaje == "html")?"checked":"";;?> name="lenguaje" value="html" id=""> <br>
+        <br>css :   <input type="radio" <?php echo ($rdg_lenguaje == "css")?"checked":"";;?> name="lenguaje" value="css" id=""> <br>
         <input type="submit" value="Enviar Informacion">
     </form>
 </body>
