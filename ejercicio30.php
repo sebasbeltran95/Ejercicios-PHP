3ecf4991ab4626d9704d77665b5cb5fabc41b249
sebastian beltran <sebastian199502@hotmail.com>
2021-12-02 15:16:49 -0500

    ejercicios

--- ejercicio30.php
+++ ejercicio30.php
@@ -1,15 +1,18 @@
 <?php
+// abrir archivos en php
 
-
+// abriendo un archivo para leer el contenido
 $archivo = "contenido.txt";
 
+// que me va a abrir el archivo con la opcion de lectua(r)
 $archivoAbierto = fopen($archivo,"r");
 
+// va a abrrir el contenido
 $contenido = fread($archivoAbierto,filesize($archivo));
 
 echo  $contenido;
 
-
+// lee el archivo y muestra el contenido
 
 
 
