b0d3f24bc71eaa8ad71c592944d9527e70e7b269
sebastian beltran <sebastian199502@hotmail.com>
2021-12-02 15:08:54 -0500

    ejercicios

---
+++ ejercicio30.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$archivo = "contenido.txt";
+
+$archivoAbierto = fopen($archivo,"r");
+
+$contenido = fread($archivoAbierto,filesize($archivo));
+
+echo $contenido;
+
+
+
+
+
+
+
+
+
+
+
+
